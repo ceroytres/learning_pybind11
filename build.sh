4de1f3c56c11d356e999d8012ddb321516fc2045
ceroytres <ceroytres@users.noreply.github.com>
2021-02-09 22:55:54 -0800

    restructure project

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ set +x
 echo $(python3-config --includes) 
 c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3-config --includes) -Iextern/pybind11/include \
                       -undefined dynamic_lookup \
-                      ./example.cpp -o example$(python3-config --extension-suffix)
+                      ./src/example.cpp -o ./build/example$(python3-config --extension-suffix)
 
 c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3-config --includes) -Iextern/pybind11/include \
                       -undefined dynamic_lookup \
-                      ./pet.cpp -o pet$(python3-config --extension-suffix)
+                      ./src/pet.cpp -o ./build/pet$(python3-config --extension-suffix)
--- src/pet.cpp
+++ src/pet.cpp
@@ -16,5 +16,8 @@ PYBIND11_MODULE(pet, m) {
     py::class_<Pet>(m, "Pet")
         .def(py::init<const std::string &>())
         .def("setName", &Pet::setName)
-        .def("getName", &Pet::getName);
+        .def("getName", &Pet::getName)
+        .def("__repr__", [](const Pet &a) {
+            return "<example.Pet named '" + a.name + "'>";
+        });
 }
